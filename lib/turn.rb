66138c54ddf6176e0167d64f3b045c7c8c64f7aa
courtney persons <personscourtney@gmail.com>
2018-09-29 15:32:25 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -23,6 +23,14 @@ def valid_move?(board, index)
     index.between?(0,8) && !position_taken?(board, index)
   end
 end
+
+def position_taken?(board, index)
+  if (board[index] == " ") || (board[index] == "") || (board [index] == nil)
+    return false
+  elsif (board[index] == "X") || (board[index] == "O")
+    return true
+  end
+end
 #   def position_taken?(array, ind)
 #     if array[ind] == " " || array[ind] == "" || array[ind] == nil
 #       return false
