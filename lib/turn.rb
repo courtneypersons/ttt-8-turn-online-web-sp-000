efe06fed254dad39a824941e60c5b6b52e6117ad
courtney persons <personscourtney@gmail.com>
2018-09-29 15:46:03 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,6 +40,9 @@ def turn(board)
 puts "Please enter 1-9:"
 user_input = gets.strip
 index = input_to_index(user_input)
+if valid_move?(board, index)
+#     move(board, index)
+#     display_board(board
 end
 #   def position_taken?(array, ind)
 #     if array[ind] == " " || array[ind] == "" || array[ind] == nil
