eadc9b73f0eb83c77446d18446d1b439613b1290
courtney persons <personscourtney@gmail.com>
2018-09-29 15:54:22 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,10 +9,49 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def valid_move?(board, index)
+  if index.between?(0,8)
+    if !position_taken?(board, index)
+      true
+    end
+  end
+
+  # position.to_i.between?(0,8) && !position_taken?(board, position.to_i-1)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def position_taken?(board, index)
+  board[index] != " "
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+
+
+
+
+
 # def input_to_index(user_input)
 # user_input.to_i - 1
 # end
-# 
+#
 # def valid_move?(board, index)
 #   def valid_move?(board, index)
 #     if index.between?(1,9)
@@ -23,7 +62,7 @@ end
 #     index.between?(0,8) && !position_taken?(board, index)
 #   end
 # end
-# 
+#
 # # def position_taken?(board, index)
 # #   if (board[index] == " ") || (board[index] == "") || (board [index] == nil)
 # #     return false
@@ -35,7 +74,7 @@ end
 # # def move(board, index, current_player)
 # # board[index] = current_player
 # # end
-# 
+#
 # def turn(board)
 #   puts "Please enter 1-9:"
 #   input = gets.strip
