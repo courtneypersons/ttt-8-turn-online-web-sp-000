cfad85a77eac96791917b723dc7f4d80adbfa181
courtney persons <personscourtney@gmail.com>
2018-09-29 15:53:50 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,44 +9,44 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-def input_to_index(user_input)
-user_input.to_i - 1
-end
-
-def valid_move?(board, index)
-  def valid_move?(board, index)
-    if index.between?(1,9)
-      if !position_taken?(board, index)
-        true
-      end
-    end
-    index.between?(0,8) && !position_taken?(board, index)
-  end
-end
-
-# def position_taken?(board, index)
-#   if (board[index] == " ") || (board[index] == "") || (board [index] == nil)
-#     return false
-#   elsif (board[index] == "X") || (board[index] == "O")
-#     return true
+# def input_to_index(user_input)
+# user_input.to_i - 1
+# end
+# 
+# def valid_move?(board, index)
+#   def valid_move?(board, index)
+#     if index.between?(1,9)
+#       if !position_taken?(board, index)
+#         true
+#       end
+#     end
+#     index.between?(0,8) && !position_taken?(board, index)
 #   end
 # end
-#
-# def move(board, index, current_player)
-# board[index] = current_player
+# 
+# # def position_taken?(board, index)
+# #   if (board[index] == " ") || (board[index] == "") || (board [index] == nil)
+# #     return false
+# #   elsif (board[index] == "X") || (board[index] == "O")
+# #     return true
+# #   end
+# # end
+# #
+# # def move(board, index, current_player)
+# # board[index] = current_player
+# # end
+# 
+# def turn(board)
+#   puts "Please enter 1-9:"
+#   input = gets.strip
+#   index = input_to_index(input)
+#   if valid_move?(board, index)
+#     move(board, index)
+#     display_board(board)
+#   else
+#     turn(board)
+#   end
 # end
-
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index)
-    display_board(board)
-  else
-    turn(board)
-  end
-end
 
 #   def position_taken?(array, ind)
 #     if array[ind] == " " || array[ind] == "" || array[ind] == nil
